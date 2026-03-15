58f9e408aa09d2dcb8cbe5677a2fbc0348030df0
BenGephardt <bengephardt@gmail.com>
2026-03-15 05:52:03 +0000

    feat: add 404 page not found route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout.jsx";
 import BrowsePage from "./pages/BrowsePage.jsx";
@@ -11,6 +12,15 @@ function App() {
         <Route path="/" element={<BrowsePage />} />
         <Route path="/my-spellbook" element={<MySpellbookPage />} />
         <Route path="/spell/:index" element={<SpellDetailPage />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h2>404: Page Not Found</h2>
+              <p>This page does not exist in the Wandering Arcanum.</p>
+            </div>
+          }
+        />
       </Route>
     </Routes>
   );
