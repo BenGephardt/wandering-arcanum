e620fe3fbfccd38cfac6369ba7d21ba5d7a94991
BenGephardt <bengephardt@gmail.com>
2026-03-15 23:34:43 +0000

    refactor: add comment to clarify purpose of useTheme hook

--- src/theme/useTheme.js
+++ src/theme/useTheme.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ThemeContext } from "./ThemeContext.js";
 
+// Povides an easy way for components to access the theme context.
 export function useTheme() {
   const ctx = useContext(ThemeContext);
   if (!ctx) {
