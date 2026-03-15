4fdcd6624e74547218e3bbd1b37f2c5ed3469e8d
BenGephardt <bengephardt@gmail.com>
2026-03-15 23:34:04 +0000

    refactor: remove redundant comment and clarify PreparedSpellsContext purpose

--- src/spellbook/PreparedSpellsContext.js
+++ src/spellbook/PreparedSpellsContext.js
@@ -1,4 +1,3 @@
-// src/spellbook/PreparedSpellsContext.js
 import { createContext } from "react";
-
+// Created to manage the state of prepared spells across the application.
 export const PreparedSpellsContext = createContext(null);
