fc6a09a217d0696d5694b788666a9deca65bf5ed
BenGephardt <bengephardt@gmail.com>
2026-03-18 20:02:21 +0000

    refactor: streamline import paths and simplify JSX structure in Providers component

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,14 +1,12 @@
-import { ThemeProvider } from "./theme/ThemeProvider.jsx";
-import { PreparedSpellsProvider } from "./spellbook/PreparedSpellsProvider.jsx";
+import { ThemeProvider } from "../theme/ThemeProvider.jsx";
+import { PreparedSpellsProvider } from "../spellbook/PreparedSpellsProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 export function Providers({ children }) {
   return (
     <ThemeProvider>
       <PreparedSpellsProvider>
-        <BrowserRouter>
-          {children}
-        </BrowserRouter>
+        <BrowserRouter>{children}</BrowserRouter>
       </PreparedSpellsProvider>
     </ThemeProvider>
   );
